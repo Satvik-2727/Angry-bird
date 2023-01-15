2c6c5a4122c734fa7005122746874c2c56cb0bd1
Satvik <techblogs2021@gmail.com>
2023-01-15 18:22:28 +0530

    Changed the caption to Angry Bird

--- main.py
+++ main.py
@@ -3,15 +3,16 @@ py.init()
 running = True
 width,height = 800,500
 screen = py.display.set_mode((width,height))
-player = py.image.load('images/Flappy bird.png')
+# player = py.image.load('images/Flappy bird.png')
 bg_img = py.image.load('images/bg.png').convert()
 bg_img = py.transform.scale(bg_img,(width,height))
+title = py.display.set_caption("Angry Bird")
 while running:
     screen.blit(bg_img,(0, 0))
     for event in py.event.get():
         if event.type == py.QUIT:
             running = False
     # screen.fill((255,255,255))
-    screen.blit(player,(370,400))
+    # screen.blit(player,(370,400))
     py.display.update()
         
