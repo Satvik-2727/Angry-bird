dd22ae76d47a56b287ad920ce9ff6f620fb506b9
Satvik <techblogs2021@gmail.com>
2023-01-15 18:09:47 +0530

    Added a background image to the screen

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
 import pygame as py
 py.init()
+running = True
+width,height = 800,500
+screen = py.display.set_mode((width,height))
+bg_img = py.image.load('images/bg.png')
+bg_img = py.transform.scale(bg_img,(width,height))
+while running:
+    screen.blit(bg_img,(0, 0))
+    for event in py.event.get():
+        if event.type == py.QUIT:
+            running = False
+    # screen.fill((255,255,255))
+    py.display.update()
+        
