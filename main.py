9b51858a00365bc1c5207ef685b15eb9cc0ee7ee
Satvik <techblogs2021@gmail.com>
2023-01-18 19:06:18 +0530

    The image would go down when no key is pressed

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ width,height = 800,500
 screen = py.display.set_mode((width,height))
 player = py.image.load('images/angry-birds.png')
 bg_img = py.image.load('images/bg.png').convert()
-bg_img = py.transform.scale(bg_img,(width,height))
+# bg_img = py.transform.scale(bg_img,(width,height))
 icon_img = py.image.load('images/icon.png')
 title = py.display.set_caption("Angry Bird")
 icon = py.display.set_icon(icon_img)
-Posx,Posy = 370,400
+Posx,Posy = 370,200
 while running:
     py.time.delay(10)
     screen.blit(player,(Posx,Posy))
@@ -21,6 +21,9 @@ while running:
     key = py.key.get_pressed()
     if key[py.K_SPACE]:
         Posy -= 10
+    else:
+        Posy += 1
+
     screen.blit(player,(Posx,Posy))        
     py.display.update()
 py.quit()
